f4dbaa113f915ff504523bd01e096863b2f5cfc5
Ben Jerke <benj427@gmail.com>
2023-08-07 11:57:21 -0400

    Recipe list wireframe exists

--- vue/src/components/RecipeList.vue
+++ vue/src/components/RecipeList.vue
@@ -1,6 +1,29 @@
 <template>
   <div>
 <Header />
+    <table>
+        <tr>
+            <th>Recipe Name</th>
+            <th>Image</th>
+        </tr>
+        <tr v-for="recipe in recipes" :key="recipe.id">
+            <td class="recipe-name">
+                <router-link :to="{}"> {{recipe.name}} </router-link> 
+            </td>
+            <td class="recipe-image-container">
+                <img class="recipe-image" :src="recipe.image" alt="">
+            </td>
+        </tr>
+
+    </table>
+
+    <form @submit="addRecipe()" class="add-recipe">
+        <input type="text" v-model="newRecipe.name" placeholder="Recipe Name">
+        <input type="text" v-model="newRecipe.instructions" placeholder="Prep Instructions">
+        <input type="text" v-model="newRecipe.image" placeholder="Image Link">
+        <input type="text" v-model="newRecipe.ingredients" placeholder="Ingredients">
+        <button>Save Recipe</button>
+    </form>
   </div>
 </template>
 

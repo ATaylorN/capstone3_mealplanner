ba8c3330f842489ab0930f706fcfb45ab444d8ab
Amanda Taylor <amandanagies@yahoo.com>
2023-08-09 09:00:00 -0400

    pulling

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,9 +3,9 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
-INSERT INTO ingredients (ingredient_id, ingredient_name, ingredient_image) VALUES (1, 'banana', 'https://images.pexels.com/photos/1166648/pexels-photo-1166648.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
-(2, 'salmon', 'https://images.pexels.com/photos/3296279/pexels-photo-3296279.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'), 
-(3, 'flour', 'https://images.pexels.com/photos/17426324/pexels-photo-17426324/free-photo-of-food-roller-on-flour-and-dough.jpeg?auto=compress&cs=tinysrgb&w=600');
+INSERT INTO ingredients (ingredient_name, ingredient_image) VALUES ('banana', 'https://images.pexels.com/photos/1166648/pexels-photo-1166648.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+('salmon', 'https://images.pexels.com/photos/3296279/pexels-photo-3296279.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'), 
+('flour', 'https://images.pexels.com/photos/17426324/pexels-photo-17426324/free-photo-of-food-roller-on-flour-and-dough.jpeg?auto=compress&cs=tinysrgb&w=600');
 
 INSERT INTO recipes(recipe_id, user_id, recipe_name, recipe_image, recipe_ingredients, instructions) VALUES (1, 1, 'Salmon-Flavored Bananas', 'https://images.pexels.com/photos/1166648/pexels-photo-1166648.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 'bananas, salmon', 'put some bananas and salmons on your plate, you got a feast its awesome');
 INSERT INTO recipes(recipe_id, user_id, recipe_name, recipe_image, recipe_ingredients, instructions) VALUES (2, 2, 'Too Many Bananas', 'https://images.pexels.com/photos/1166648/pexels-photo-1166648.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 'bananas', 'just a ton of bananas on a plate, you do you, I guess');

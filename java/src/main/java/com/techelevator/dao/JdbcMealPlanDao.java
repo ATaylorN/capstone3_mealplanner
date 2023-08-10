3ab973f31a13e9488072fd1736ef1028251867ac
Robert Carter <racarter677@gmail.com>
2023-08-10 15:26:56 -0400

    pushing meal plan jdbc

--- java/src/main/java/com/techelevator/dao/JdbcMealPlanDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcMealPlanDao.java
@@ -43,32 +43,104 @@ public class JdbcMealPlanDao implements MealPlanDAO {
 
     @Override
     public List<MealPlan> getFutureUserMealPlans(int userid) {
-        return null;
+        List<MealPlan> mealPlans = new ArrayList<>();
+        LocalDate currentDate = LocalDate.now();
+        String sql = "SELECT meal_plan_id, user_id, meal_id, plan_date " +
+                "FROM meal_plans " +
+                "WHERE user_id = ? " +
+                "AND plan_date >= ?;";
+        try {
+            SqlRowSet rows = jdbcTemplate.queryForRowSet(sql, userid, currentDate);
+            while (rows.next()){
+                mealPlans.add(mapRowToMealPlan(rows));
+            }
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new RuntimeException("Unable to connect to the database.", e);
+        } catch (DataIntegrityViolationException e) {
+            throw new RuntimeException("Action would violate data integrity.", e);
+        } catch (BadSqlGrammarException e) {
+            throw new RuntimeException("Invalid syntax.", e);
+        }
+        return mealPlans;
     }
 
     @Override
     public MealPlan getMealPlanById(int mealPlanId) {
-        return null;
+        MealPlan mealPlan = null;
+        String sql = "SELECT meal_plan_id, user_id, meal_id, plan_date " +
+                "FROM meal_plans " +
+                "WHERE meal_plan_id = ?;";
+        try {
+            SqlRowSet rows = jdbcTemplate.queryForRowSet(sql, mealPlanId);
+            if (rows.next()) {
+                mealPlan = mapRowToMealPlan(rows);
+            }
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new RuntimeException("Unable to connect to the database.", e);
+        } catch (DataIntegrityViolationException e) {
+            throw new RuntimeException("Action would violate data integrity.", e);
+        } catch (BadSqlGrammarException e) {
+            throw new RuntimeException("Invalid syntax.", e);
+        }
+        return mealPlan;
     }
 
     @Override
     public List<MealPlan> getUserMealPlansForDateRange(LocalDate startDate, LocalDate endDate) {
-        return null;
+        List<MealPlan> mealPlans = new ArrayList<>();
+        String sql = "SELECT meal_plan_id, user_id, meal_id, plan_date " +
+                "FROM meal_plans " +
+                "WHERE plan_date >= ? AND plan_date <= ?";
+        try {
+            SqlRowSet rows = jdbcTemplate.queryForRowSet(sql, startDate, endDate);
+            while (rows.next()){
+                mealPlans.add(mapRowToMealPlan(rows));
+            }
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new RuntimeException("Unable to connect to the database.", e);
+        } catch (DataIntegrityViolationException e) {
+            throw new RuntimeException("Action would violate data integrity.", e);
+        } catch (BadSqlGrammarException e) {
+            throw new RuntimeException("Invalid syntax.", e);
+        }
+        return mealPlans;
     }
 
     @Override
     public int addMealPlan(MealPlan mealPlan) {
-        return 0;
+        String sql = "INSERT INTO INSERT INTO meal_plans (user_id, meal_id, plan_date) " +
+                "VALUES (?, ?, ?) RETURNING meal_plan_id";
+        int mealPlanId;
+        try {
+            mealPlanId = jdbcTemplate.queryForObject(sql, int.class, mealPlan.getUserId(), mealPlan.getPlannedMealId(), mealPlan.getDateToCook());
+            mealPlan.setId(mealPlanId);
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new RuntimeException("Unable to connect to the database.", e);
+        } catch (DataIntegrityViolationException e) {
+            throw new RuntimeException("Action would violate data integrity.", e);
+        } catch (BadSqlGrammarException e) {
+            throw new RuntimeException("Invalid syntax.", e);
+        }
+        return mealPlanId;
     }
 
     @Override
     public int getIngredientsByMealPlan(List<Ingredient> ingredients) {
         return 0;
-    }
+    } 
 
     @Override
-    public void deleteMealPlan() {
-
+    public void deleteMealPlan(MealPlan mealPlan) {
+        String sql = "DELETE FROM meal_plans WHERE meal_plan_id = ?";
+        try {
+            jdbcTemplate.update(sql, mealPlan.getId());
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new RuntimeException("Unable to connect to the database.", e);
+        } catch (DataIntegrityViolationException e) {
+            throw new RuntimeException("Action would violate data integrity.", e);
+        } catch (BadSqlGrammarException e) {
+            throw new RuntimeException("Invalid syntax.", e);
+        }
     }
 
     private MealPlan mapRowToMealPlan(SqlRowSet rows){

44cfc620ec1344f1e6bf6c02286320cdf36dd82d
Sheena McGrath <sheenzesez@gmail.com>
2023-08-14 10:10:01 -0400

    grocery list component/view/service skeletons

---
+++ vue/src/components/GroceryList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <Header />
+  </div>
+</template>
+
+<script>
+import Header from './Header.vue'
+export default {
+    name: "grocery-list",
+    components: {
+        Header
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/src/views/GroceryList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <GroceryList />
+  </div>
+</template>
+
+<script>
+import GroceryList from '../components/GroceryList.vue'
+export default {
+    components: {
+        GroceryList
+    }
+}
+</script>
+
+<style>
+
+</style>

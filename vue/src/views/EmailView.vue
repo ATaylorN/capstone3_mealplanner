ec79ab55ed50ce6b80299f4506bbb8d3a91c2431
Amanda Taylor <amandanagies@yahoo.com>
2023-08-15 10:55:42 -0400

    midday merge

--- vue/src/views/EmailView.vue
+++ vue/src/views/EmailView.vue
@@ -1,35 +1,39 @@
 <template>
 <div>
     <Header />
- 
-  <div class="email-form">
-   <form id="contact-form">
-        <input type="hidden" name="contact_number">
-        <label>Name</label>
-        <input type="text" name="user_name">
-        <label>Email</label>
-        <input type="email" name="user_email">
-        <label>Message</label>
-        <textarea name="message"></textarea>
-        <input type="submit" value="Send">
-    </form>
-  </div>
+ <div class="email-form">
+  <form ref="form" @submit.prevent="sendEmail">
+    <label>Name</label>
+    <input type="text" name="user_name">
+    <label>Email</label>
+    <input type="email" name="user_email">
+    <label>Message</label>
+    <textarea name="message"></textarea>
+    <input type="submit" value="Send">
+  </form>
+</div>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue';
+import emailjs from '@emailjs/browser';
 
 export default {
     name: "email-view",
     components: {
         Header
     },
-    data(){
-        return{
-            messageBody: ""
-        }
+      methods: {
+    sendEmail() {
+      emailjs.sendForm('service_8vy3gma', 'template_x42f4g7', this.$refs.form, 'GEie82de2ImMbutSn')
+        .then((result) => {
+            console.log('SUCCESS!', result.text);
+        }, (error) => {
+            console.log('FAILED...', error.text);
+        });
     }
+  }
 }
 </script>
 
